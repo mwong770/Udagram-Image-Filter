6e3ef488041eaaa4b8b8c4553ba850ece4dd0e0a
Maria Wong <mwong771@yahoo.com>
2019-11-15 23:59:15 -0500

    adds get filteredimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import validUrl from 'valid-url';
 
 (async () => {
 
@@ -9,34 +10,48 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage/", async (req, res) => {
 
-  /**************************************************************************** */
+    let { image_url } = req.query;
 
-  //! END @TODO1
-  
+    // validates the image_url query
+    if (!validUrl.isUri(image_url)) {
+        return res.status(400).send("Please provide a valid URL.");
+    }
+    if (!(image_url.includes(".png") || image_url.includes(".jpg"))) {
+        return res.status(422).send("The provided URL must contain a .png or .jpg extension.");
+    }
+
+    try {
+        // filters the image
+        filterImageFromURL(image_url).then(filteredpath => {
+            // sends filtered image in response
+            res.status(200).sendFile(filteredpath, (error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    // deletes filtered image files
+                    deleteLocalFiles([filteredpath]);
+                }
+            });
+
+        });
+    } catch (error) {
+        res.sendStatus(422).send("An error occurred while processing the provided URL.");
+    }
+
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
